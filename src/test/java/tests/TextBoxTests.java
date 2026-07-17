b82b43358928f67e843806802176e563b98d5f44
ea.polyakova <polyakova.ea@inbox.ru>
2026-07-17 17:21:25 +0300

    вводимые и проверяемые значения вынесены в переменные

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -18,63 +19,89 @@ public class TextBoxTests {
 
     @Test
     void successfulFillFormTest() {
+        String firstName = "Елена";
+        String lastName = "Полякова";
+        String name = firstName + " " + lastName;
+        String userEmail = "elena@yandex.ru";
+        String userNumber = "9162223344";
+        String genter = "Female";
+        String hobby = "Music";
+        String currentAddress = "My address";
+        String month = "May";
+        String year = "1999";
+        String data = "20 May,1999";
+
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Елена");
-        $("[id=lastName]").setValue("Полякова");
-        $("[id=userEmail]").setValue("elena@yandex.ru");
-        $("[id=genterWrapper]").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("9162223344");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=userEmail]").setValue(userEmail);
+        $("[id=genterWrapper]").$(byText(genter)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("[id=dateOfBirthInput]").click();
-        $(".react-datepicker__month-select").selectOption("May");
-        $(".react-datepicker__year-select").selectOption("1999");
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day.react-datepicker__day--020").click();
-        $("#hobbiesWrapper").$(byText("Music")).click();
+        $("#hobbiesWrapper").$(byText(hobby)).click();
         $("[id=uploadPicture]").uploadFromClasspath("picture.jpg");
-        $("[id=currentAddress]").setValue("My address");
+        $("[id=currentAddress]").setValue(currentAddress);
         $("[id='react-select-3-input']").setValue("NCR").pressEnter();
         $("[id='react-select-4-input']").setValue("Delhi").pressEnter();
         $("#submit").click();
 
         $(".modal-content").shouldBe(visible);
         $(".table-responsive").shouldHave(text("Student Name"));
-        $(".table-responsive").shouldHave(text("Елена Полякова"));
-        $(".table-responsive").shouldHave(text("elena@yandex.ru"));
-        $(".table-responsive").shouldHave(text("Female"));
-        $(".table-responsive").shouldHave(text("9162223344"));
+        $(".table-responsive").shouldHave(text(name));
+        $(".table-responsive").shouldHave(text(userEmail));
+        $(".table-responsive").shouldHave(text(genter));
+        $(".table-responsive").shouldHave(text(userNumber));
         $(".table-responsive").shouldHave(text("Subjects"));
-        $(".table-responsive").shouldHave(text("Music"));
-        $(".table-responsive").shouldHave(text("20 May,1999"));
-        $(".table-responsive").shouldHave(text("My address"));
+        $(".table-responsive").shouldHave(text(hobby));
+        $(".table-responsive").shouldHave(text(data));
+        $(".table-responsive").shouldHave(text(currentAddress));
         $(".table-responsive").shouldHave(text("NCR Delhi"));
         closeWebDriver();
     }
 
     @Test
     void successfullCompletedWithOnlyRequiredFieldTest() {
+        String firstName = "Елена";
+        String lastName = "Полякова";
+        String name = firstName + " " + lastName;
+        String userNumber = "9162223344";
+        String genter = "Female";
+
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Елена");
-        $("[id=lastName]").setValue("Полякова");
-        $("[id=genterWrapper]").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("9162223344");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=genterWrapper]").$(byText(genter)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("[id=submit]").click();
+
         $(".modal-content").shouldBe(visible);
-        $(".table-responsive").shouldHave(text("Елена Полякова"));
-        $(".table-responsive").shouldHave(text("Female"));
-        $(".table-responsive").shouldHave(text("9162223344"));
+        $(".table-responsive").shouldHave(text(name));
+        $(".table-responsive").shouldHave(text(genter));
+        $(".table-responsive").shouldHave(text(userNumber));
         closeWebDriver();
     }
 
     @Test
     void shortEmailTest() {
+        String firstName = "Елена";
+        String lastName = "Полякова";
+        String userNumber = "9162223344";
+        String genter = "Female";
+        String month = "May";
+        String year = "1999";
+
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Елена");
-        $("[id=lastName]").setValue("Полякова");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
         $("[id=userEmail]").setValue("ele");
-        $("[id=genterWrapper]").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("9162223344");
+        $("[id=genterWrapper]").$(byText(genter)).click();
+        $("[id=userNumber]").setValue(userNumber);
         $("[id=dateOfBirthInput]").click();
-        $(".react-datepicker__month-select").selectOption("May");
-        $(".react-datepicker__year-select").selectOption("1999");
+        $(".react-datepicker__month-select").selectOption(month);
+        $(".react-datepicker__year-select").selectOption(year);
         $(".react-datepicker__day.react-datepicker__day--020").click();
         $("#submit").click();
 
@@ -84,9 +111,12 @@ public class TextBoxTests {
 
     @Test
     void wrongEmailTest() {
+        String wrongEmail = "Елена@yandex.ru";
+
         open("/automation-practice-form");
-        $("[id=userEmail]").setValue("Елена@yandex.ru");
+        $("[id=userEmail]").setValue(wrongEmail);
         $("[id=submit]").click();
+
         $(".modal-content").shouldNotBe(visible);
         $("[id=userEmail]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         closeWebDriver();
@@ -95,9 +125,12 @@ public class TextBoxTests {
 
     @Test
     void wrongNumberTest() {
+        String wrongNumber = "вв";
+
         open("/automation-practice-form");
-        $("[id=userNumber]").setValue("вв");
+        $("[id=userNumber]").setValue(wrongNumber);
         $("[id=submit]").click();
+
         $(".modal-content").shouldNotBe(visible);
         $("[id=userNumber]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         closeWebDriver();
@@ -105,12 +138,18 @@ public class TextBoxTests {
 
     @Test
     void emptyNumberTest() {
+        String firstName = "Елена";
+        String lastName = "Полякова";
+        String genter = "Female";
+        String emptyNumber = "";
+
         open("/automation-practice-form");
-        $("[id=firstName]").setValue("Елена");
-        $("[id=lastName]").setValue("Полякова");
-        $("[id=genterWrapper]").$(byText("Female")).click();
-        $("[id=userNumber]").setValue("");
+        $("[id=firstName]").setValue(firstName);
+        $("[id=lastName]").setValue(lastName);
+        $("[id=genterWrapper]").$(byText(genter)).click();
+        $("[id=userNumber]").setValue(emptyNumber);
         $("[id=submit]").click();
+
         $(".modal-content").shouldNotBe(visible);
         $("[id=userNumber]").shouldHave(cssValue("border-color", "rgb(220, 53, 69)"));
         closeWebDriver();
